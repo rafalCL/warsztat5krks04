4656cd8b1a7f3aac193fe2bd7cf45feaf36a6dd0
rafalcl <rafalclab@gmail.com>
2018-10-12 12:26:07 +0200

    Initialize Spring Web Mvc support

---
+++ src/main/java/pl/coderslab/warsztat5krks04/app/AppConfig.java
@@ -0,0 +1,12 @@
+package pl.coderslab.warsztat5krks04.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"pl.coderslab.warsztat5krks04"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+}
---
+++ src/main/java/pl/coderslab/warsztat5krks04/controller/BookController.java
@@ -0,0 +1,14 @@
+package pl.coderslab.warsztat5krks04.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "{msg: hello}";
+    }
+}
